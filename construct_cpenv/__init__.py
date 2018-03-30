987f25f307b3a3edb501705acab2ba791a890382
Dan Bradham <danielbradham@gmail.com>
2018-03-30 18:47:49 -0400

    Fix pkg metadata

--- construct_cpenv/__init__.py
+++ construct_cpenv/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-__title__ = 'constrtuct_cpenv'
+__title__ = 'construct_cpenv'
 __description__ = 'Construct Cpenv Integration'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Dan Bradham'
 __email__ = 'danielbradham@gmail.com'
 __license__ = 'MIT'
-__url__ = 'https://github.com/danbradham/constrtuct_cpenv'
+__url__ = 'https://github.com/construct-org/construct_cpenv'
 
 from construct_cpenv.actions import *
 from construct_cpenv.extensions import *
